824625a9b9bfc4cc3c5bfeb190df602ff47d0b9b
akelstrom <akelstrom@gmail.com>
2020-11-28 20:59:06 -0500

    added taco marker for map

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,11 +1,20 @@
 mapsApiKey = "AIzaSyDZf8YFMxQn0Vz68m7s7x9eWssFYa9GeIQ"
 
-let map;
-
 function initMap() {
+    //map options
 var options = {
-    zoom: 8,
-    center: {lat: 39.9526 ,lng: 75.1652 }
+    zoom: 10,
+    center: {lat: 39.9526 ,lng: -75.1652 }
 }
+
+//new map
 var map = new google.maps.Map(document.getElementById('map'),options);
+
+//add marker
+var marker = new google.maps.Marker({
+    position: {lat: 39.9526 ,lng: -75.1652},
+    map: map,
+    icon: 'https://img.icons8.com/color/48/000000/taco.png'
+});
 }
+
