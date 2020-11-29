fbf1eb0160bec4434a50dda26b836e5cb99b104c
akelstrom <akelstrom@gmail.com>
2020-11-28 20:44:56 -0500

    got a map to display, updated html for map container, css for map styling, and js

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,2 +1,11 @@
 mapsApiKey = "AIzaSyDZf8YFMxQn0Vz68m7s7x9eWssFYa9GeIQ"
 
+let map;
+
+function initMap() {
+var options = {
+    zoom: 8,
+    center: {lat: 39.9526 ,lng: 75.1652 }
+}
+var map = new google.maps.Map(document.getElementById('map'),options);
+}
