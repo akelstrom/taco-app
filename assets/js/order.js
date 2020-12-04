c7482eb1a331e3846de5ef008b6d8718fd3e3889
akelstrom <akelstrom@gmail.com>
2020-12-03 21:28:54 -0500

    added info window function- needs error handling

--- assets/js/order.js
+++ assets/js/order.js
@@ -13,32 +13,30 @@ actionBtn.addEventListener("click", getLocation);
 //function that get users approval to get user current location
 function getLocation() {
   if (navigator.geolocation) {
-
-    // call Materialize toast to update user *!!why does this appear before geolocation is confirmed??
-    mapArea.innerHTML = M.toast({ html: 'fetching your current location', classes: 'rounded red lighten-1' });
-    
+    // // call Materialize toast to update user *!!why does this appear before geolocation is confirmed??
+    // M.toast({ html: 'fetching your current location', classes: 'rounded red lighten-1' });
     navigator.geolocation.getCurrentPosition(displayLocation, showError);
   }
 }
 
 // Displays the different error messages- change alerts to modals
 showError = (error) => {
-    mapArea.style.display = "block"
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
-       alert("You denied the request for your location.") 
-        break;
-      case error.POSITION_UNAVAILABLE:
-        alert("Your Location information is unavailable.")
-        break;
-      case error.TIMEOUT:
-        alert("Your request timed out. Please try again")
-        break;
-      case error.UNKNOWN_ERROR:
-        alert("An unknown error occurred please try again after some time.")
-        break;
-    }
-  };
+  mapArea.style.display = "block";
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      alert("You denied the request for your location.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      alert("Your Location information is unavailable.");
+      break;
+    case error.TIMEOUT:
+      alert("Your request timed out. Please try again");
+      break;
+    case error.UNKNOWN_ERROR:
+      alert("An unknown error occurred please try again after some time.");
+      break;
+  }
+};
 
 //function that gets the users location and put's their lat and long into a variable we can pass into map
 function displayLocation(position) {
@@ -55,7 +53,7 @@ function displayLocation(position) {
 function initMap(latlng) {
   var options = {
     zoom: 14,
-    center: latlng
+    center: latlng,
   };
 
   map = new google.maps.Map(document.getElementById("map"), options);
@@ -64,37 +62,50 @@ function initMap(latlng) {
     location: latlng,
     radius: "1000",
     type: ["restaurant"],
-    keyword: "mexican"
+    keyword: "mexican",
   };
 
   service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, createMarkers);
-  
 }
 
 //function that handles how the markers work-- need to fix this- why aren't the markers clickable
 function createMarkers(results, status) {
-    
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       let markerOptions = {
         position: results[i].geometry.location,
         title: "hello",
         clickable: true,
-        // animation: google.maps.Animation.BOUNCE, 
-        icon:"https://img.icons8.com/color/48/000000/taco.png"        
+        // animation: google.maps.Animation.BOUNCE,
+        icon: "https://img.icons8.com/color/48/000000/taco.png",
       };
-
 
       marker = new google.maps.Marker(markerOptions);
       marker.setMap(map);
 
+      console.log(results);
+
+//connect event listener to a function that displays results data
+//getting error at this point.
+      marker.addListener("click", displayData(results, i, map, marker));
     }
   }
 }
 
-//event listener for dropdown nav bar
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+function displayData(results, position, map, marker) {
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    position: results[position].geometry.location
   });
+
+  infowindow.open(map);
+
+  var contentString = `<h4> ${results[position].name}</h4>`;
+}
+
+// //event listener for dropdown nav bar
+// document.addEventListener("DOMContentLoaded", function () {
+//   var elems = document.querySelectorAll(".sidenav");
+//   var instances = M.Sidenav.init(elems, options);
+// });
