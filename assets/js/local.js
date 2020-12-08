c58cc0aaa0609421cdf9d33033e874b5628af5a6
Veronica Martinez <vmartinez6615@gmail.com>
2020-12-08 16:21:52 -0500

    finished with styling

--- assets/js/local.js
+++ assets/js/local.js
@@ -1,28 +1,26 @@
 var faveSpot = document.getElementById("getFaveSpot");
 
-//load saved food places from local storage
 var foodPlaces = JSON.parse(localStorage.getItem("faveLocation"));
 
 function formatPlace(data) {
-  // displays restaurants
   return `<div class="row card-row">
   <div class="col s12 m6">
     <div class="card">
       <div class="card-image">
       <img src="${data.photo}">
-        <h6 class="bg-light">${data.name}</h6>
+        <h6 class="card-title red lighten-1">${data.name}</h6>
       </div>
       <div class="card-content">
         <p>Address: ${data.address}</p>
-        <p>Phone Number: ${data.phoneNumber}</p>
-        <p>Website: <a href="${data.website}">Click Here</a></p>
+        <p>Phone Number:<a href="tel:${data.phoneNumber}"><i class="material-icons tiny">call</i>
+        </a> ${data.phoneNumber}</p>
         <p>Rating: ${data.rating}</p>
+        <p><a href="${data.website}">Link to Website</a></p>
       </div>
     </div>
   </div>
 </div>`;
 }
-
 var loadFoodPlaces = function () {
   for (let i = 0; i < foodPlaces.length; i++) {
     var restaurantList = document.createElement("li");
